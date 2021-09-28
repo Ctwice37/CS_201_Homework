7908ead1f076cdf837cf399a3098230879a1a6fb
Charles C <carpe359@gmail.com>
2021-09-28 17:20:01 -0500

    Update name_initial.java

--- src/lab_2/name_initial.java
+++ src/lab_2/name_initial.java
@@ -1,10 +1,15 @@
+//question 1
+
 package lab_2;
 import java.util.Scanner;
+
 public class name_initial {
     public static void main(String[] args) {
+        
         Scanner input = new Scanner(System.in);
 
         System.out.println("Input your First, Middle, and Last Name: ");
+        
         String first = input.next();
         String middle = input.next();
         String last = input.next();
