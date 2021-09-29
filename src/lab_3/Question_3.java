f4a2cdce9a4f70c93cefab87ee46d4b7fc844995
Charles Carpenter <carpe359@gmail.com>
2021-09-29 16:29:55 -0500

    Lab_2 Classes

--- src/lab_3/Question_3.java
+++ src/lab_3/Question_3.java
@@ -1,2 +1,18 @@
-package lab_3;public class Question_3 {
+package lab_3;
+
+import java.util.Scanner;
+
+public class Question_3 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Give celsius temperature: ");
+
+        convertCtoF(input.nextInt());
+    }
+    public static void convertCtoF(int temp){
+        double f = 1.8 * temp + 32;
+        System.out.println(temp + " Celsius is " + f + " Fahrenheit");
+
+    }
 }
