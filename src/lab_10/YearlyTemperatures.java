27147c9eaf77fd784d1cbaac6f7090061fbf8268
Charles Carpenter <carpe359@gmail.com>
2021-10-12 18:45:56 -0500

    10/12/21

--- src/lab_10/YearlyTemperatures.java
+++ src/lab_10/YearlyTemperatures.java
@@ -1,5 +1,7 @@
 package lab_10;
 
+import java.util.Scanner;
+
 public class YearlyTemperatures {
     static double hottestTempYear = 0;
     static double coldestTempYear = 0;
@@ -11,7 +13,9 @@ public class YearlyTemperatures {
     static String[] months = {"January", "February", "March", "April", "May",
             "June", "July", "August", "September", "October", "November", "December"};
     static int[] monthLength = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
     static double[] avgMonthTemp = new double[12];
+    static double[] tempMonthlyDiff = new double[12];
     static double[] TempsYearly = new double[365];
 
 
@@ -21,7 +25,7 @@ public class YearlyTemperatures {
 
         }
         double [] temps = AvgTempMonth(TempsYearly);
-            for(double month : TempsYearly)
+            for(double month : avgMonthTemp)
                 System.out.println(month);
 
 
@@ -41,26 +45,63 @@ public class YearlyTemperatures {
 
         public static double [] AvgTempMonth(double [] temperatures){
 
-            double sum = 0;
-            double average = 0;
-            int dayOfYear = 0;
+       double average = 0;
+       double sum = 0;
+       int month = 0;
+       int start = 0;
 
-            for (int c = 0; c < 12; c++) {
-                dayOfYear = monthLength[c];
-                dayOfYear = temperatures[]
-                for (int r = dayOfYear; r monthLength.length; r++) {
-                    sum += temperatures[r];
+       while (month < 12){
+           for (int i = start; i < monthLength[month] + start; i++) {
+               sum += TempsYearly[i];
+           }average = sum / monthLength[month];
+           avgMonthTemp[month]  = average;
+           sum = 0;
+           average = 0;
+           start += monthLength[month];
+           month++;
 
-
-                } average = sum / monthLength[c];
-                avgMonthTemp[c] = average;
-                sum = 0.0;
-                average = 0;
+           }return avgMonthTemp;
 
 
             }
 
+        public static double [] monthMaxMin(double [] temperatures){
+            double max = 0;
+            double min = 0;
+            int month = 0;
+            int start = 0;
+            double difference = 0;
 
-            } return avgMonthTemp;
+            while (month < 12){
+                for (int i = start; i < monthLength[month] + start; i++) {
+                    if(temperatures[i] > max)
+                        max = temperatures[i];
+                    else if (temperatures[i] < min)
+                        min = temperatures[i];
+
+                }difference = max - min;
+                tempMonthlyDiff[month]  = difference;
+                max = 0;
+                min = 0;
+                start += monthLength[month];
+                month++;
+
+            }return tempMonthlyDiff;
+
         }
-}
+
+        public static double tempOfDayOfMonth(int month, int day){
+            Scanner input = new Scanner(System.in);
+            System.out.println("Temperature in What Month? ");
+            int month = input.nextInt();
+            System.out.println("Temperature on what day? ");
+            int day = input.nextInt();
+
+
+
+
+        }
+
+            }
+
+
